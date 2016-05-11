1716ff3ace2e05f67c7a3bfa72df49da11e6264c
Jan Kowalski <test@o2.pl>
2016-05-11 12:07:27 +0200

    Add error files to main folder

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -3,22 +3,30 @@ describe('app', function () {
 
     var app = window.app;
 
-    describe('assignToSwimmingCourse', function () {
-        it('should assign adult person to adult group', function () {
+    xxdescribe('assignToSwimmingCourse', function () {
+        xit('should assign adult person to adult group', function () {
             expect(app.assignToSwimmingCourse('Jack', '10/10/1995')).toEqual({
-                name: 'Jack', age: 20, course: 'adults'
+                name: 'John", age: 20, course: 'adults'
             });
         });
-        it('should assign person between the ages of 12 - 17 to teens group', function () {
+        iit('should assign person between the ages of 12 - 17 to teens group', function () {
             expect(app.assignToSwimmingCourse('John', '07/10/2001')).toEqual({
-                name: 'John', age: 14, course: 'teens'
+                name: ", age: 14, course: 'teens'
             });
         });
         it('should assign person below 12 to kids group', function () {
             expect(app.assignToSwimmingCourse('Zack', '10/06/2010')).toEqual({
                 name: 'Zack', age: 5, course: 'kids'
             });
-        });
+        })
+        it('should assign person below 12 to kids group', function () {
+            expect(app.assignToSwimmingCourse('Zack', '10/06/2010')).toEqual({
+                name: 'John', age: 0, course: '5'
+            }))
+
+        it('shouldnt assign person below 12 to kids groupe', function())    {
+
+            }
     });
 });
 
